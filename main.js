9892a831c9aeff6b8ac36d30ff0a7c2dd8f58c89
huibin <773294296@qq.com>
2019-08-22 21:30:45 +0800

    8.22.21.30

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ const path = require('path')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let win
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -20,46 +20,53 @@ function createWindow() {
     }
   })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+
+  // and load the index.html of the app.加载index.html文件
+  win.loadFile('index.html')
+  // win.loadURL('https://github.com')
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  win.webContents.openDevTools()
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  // Emitted when the window is closed.当 window 被关闭，这个事件会被触发。
+  win.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+    // 取消引用 window 对象，如果你的应用支持多窗口的话，
+    // 通常会把多个 window 对象存放在一个数组里面，
+    // 与此同时，你应该删除相应的元素。
+    win = null
   })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
+// Electron 会在初始化后并准备
+// 创建浏览器窗口时，调用这个函数。
+// 部分 API 在 ready 事件触发后才能使用。
 app.on('ready', createWindow)
 
-app.on('ready', () => {
-  // Register a 'CommandOrControl+Y' shortcut listener.
-  globalShortcut.register('CommandOrControl+Y', () => {
-    // Do stuff when Y and either Command/Control is pressed.
-    console.log(444)
-  })
-})
-
 // Quit when all windows are closed.
+// 当全部窗口关闭时退出。
 app.on('window-all-closed', function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
+  // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+  // 否则绝大部分应用及其菜单栏会保持激活。
   if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
+  // 在macOS上，当单击dock图标并且没有其他窗口打开时，
+  // 通常在应用程序中重新创建一个窗口。
+  if (win === null) createWindow()
 })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+// 在这个文件中，你可以续写应用剩下主进程代码。
+// 也可以拆分成几个文件，然后用 require 导入。
--- renderer.js
+++ renderer.js
@@ -1,3 +1,18 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+//该文件是index.html文件所必需的，并且将
+
+//在该窗口的渲染进程中执行。
+
+//所有node.js API在此过程中都可用。
+let myNotification = new Notification('标题', {
+  body: '通知正文内容'
+})
+myNotification.onclick = () => {
+  console.log('通知被点击')
+}
+
+
+const { app } = require('electron').remote
+console.log(app.getVersion())
